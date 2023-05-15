9d8da41c4bad469c9bc1b00236b178e2d3771493
BrandLop <brandon.lopezt@alumno.buap.mx>
2023-05-14 21:44:02 -0600

    Integrate qr access screen and home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:buapmovil/screens/qr_acc_screen.dart';
 import 'package:buapmovil/views/degree_map.dart';
+import 'package:buapmovil/views/home.dart';
 import 'package:buapmovil/views/kardex.dart';
 import 'package:buapmovil/views/my_schedule.dart';
 import 'package:buapmovil/views/news_paper.dart';
@@ -17,14 +19,13 @@ class MyApp extends StatelessWidget {
       title: 'BUAP Movil',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.white,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: const Color(0xFF003B5C),
-          secondary: const Color(0xFF00B5E2),
-        ),
-        textTheme: GoogleFonts.sourceSansProTextTheme(),
-        visualDensity: VisualDensity.comfortable
-      ),
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color(0xFF003B5C),
+            secondary: const Color(0xFF00B5E2),
+          ),
+          textTheme: GoogleFonts.sourceSansProTextTheme(),
+          visualDensity: VisualDensity.comfortable),
       home: const MyHomePage(),
     );
   }
@@ -40,8 +41,9 @@ class MyHomePage extends StatefulWidget {
 class MyHomePageState extends State<MyHomePage> {
   String appBarText = 'BUAP Móvil';
 
-  int _currentIndex = 2;
+  int _currentIndex = 0;
   final List<Widget> _views = [
+    const HomeView(),
     const DegreeMapView(),
     const NewspaperView(),
     const ScheduleView(),
@@ -70,19 +72,21 @@ class MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text(appBarText),
-        titleTextStyle: GoogleFonts.montserrat(
-          fontWeight: FontWeight.w900,
-          fontSize: 24
-        ),
+        titleTextStyle:
+            GoogleFonts.montserrat(fontWeight: FontWeight.w900, fontSize: 24),
         actions: [
-          PopupMenuButton(
-            itemBuilder: (context) {
-              return const [
-                PopupMenuItem(child: Center(child: Text('Acerca de', style: TextStyle(fontSize: 14)))),
-                PopupMenuItem(child: Center(child: Text('Cerrar sesión', style: TextStyle(fontSize: 14))))
-              ];
-            }
-          )
+          PopupMenuButton(itemBuilder: (context) {
+            return const [
+              PopupMenuItem(
+                  child: Center(
+                      child:
+                          Text('Acerca de', style: TextStyle(fontSize: 14)))),
+              PopupMenuItem(
+                  child: Center(
+                      child: Text('Cerrar sesión',
+                          style: TextStyle(fontSize: 14))))
+            ];
+          })
         ],
       ),
       body: IndexedStack(
@@ -90,40 +94,49 @@ class MyHomePageState extends State<MyHomePage> {
         children: _views,
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: _onTabSelected,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.timeline_rounded),
-            label: 'Mi carrera',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.newspaper),
-            label: 'Noticias',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: 'Mi horario',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.location_pin),
-            label: 'Lobo Guia',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.contact_page),
-            label: 'Mi kardex',
-          ),
-        ],
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        unselectedItemColor: Colors.grey,
-        selectedLabelStyle: const TextStyle(fontSize: 12)
-      ),
+          currentIndex: _currentIndex,
+          onTap: _onTabSelected,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_filled),
+              label: 'Inicio',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.edit),
+              label: 'Mi carrera',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.newspaper),
+              label: 'Noticias',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: 'Mi horario',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.location_pin),
+              label: 'Lobo Guia',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.contact_page),
+              label: 'Mi kardex',
+            ),
+          ],
+          selectedItemColor: Theme.of(context).colorScheme.primary,
+          unselectedItemColor: Colors.grey,
+          selectedLabelStyle: const TextStyle(fontSize: 12)),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Acción al presionar el botón flotante
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const WebViewScreen(),
+              ));
         },
         backgroundColor: Theme.of(context).colorScheme.secondary,
-        child: const Icon(Icons.qr_code_2,),
+        child: const Icon(
+          Icons.qr_code_2,
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
